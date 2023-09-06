6edc442b45c964391bf96f80e1d61c8f73a13e3e
Bharath Nair <nairbharath007@gmail.com>
2023-09-07 00:00:44 +0530

    modularity to AccountController functions and class library implementation

--- AccountPresentationAppFramework/AccountController.cs
+++ AccountPresentationAppFramework/AccountController.cs
@@ -25,29 +25,7 @@ namespace AccountPresentationAppFramework
             if (account == null)
             {
                 Console.WriteLine("Welcome to the Account Application!");
-                Console.Write("Enter your Account Number: ");
-                int accountNumber;
-                while (!int.TryParse(Console.ReadLine(), out accountNumber))
-                {
-                    Console.WriteLine("Invalid Account Number. Please enter a valid number.");
-                }
-
-                Console.Write("Enter your Account Holder Name: ");
-                string accountHolderName = Console.ReadLine();
-
-                Console.Write("Enter your Bank Name: ");
-                string bankName = Console.ReadLine();
-
-                double openingBalance;
-                do
-                {
-                    Console.Write("Enter Opening Balance (minimum 500 rupees): ");
-                } while (!double.TryParse(Console.ReadLine(), out openingBalance) || openingBalance < 500);
-
-                account = new Account(accountNumber, accountHolderName, bankName, openingBalance);
-                AccountManager.SaveAccount(account);
-
-                Console.WriteLine("Account created successfully!");
+                CreateAccount();
             }
             else
             {
@@ -55,75 +33,120 @@ namespace AccountPresentationAppFramework
                 Console.WriteLine($"Account Balance: {account.Balance}");
             }
 
-            int choice;
+            while (true)
+            {
+                PrintMenu();
+                int choice = GetUserChoice();
+
+                switch (choice)
+                {
+                    case 1:
+                        DepositMoney(account);
+                        break;
+                    case 2:
+                        WithdrawMoney(account);
+                        break;
+                    case 3:
+                        DisplayBalance(account);
+                        break;
+                    case 4:
+                        Console.WriteLine("Exiting the application.");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        private void CreateAccount()
+        {
+            Console.Write("Enter your Account Number: ");
+            int accountNumber;
+            while (!int.TryParse(Console.ReadLine(), out accountNumber))
+            {
+                Console.WriteLine("Invalid Account Number. Please enter a valid number.");
+            }
+
+            Console.Write("Enter your Account Holder Name: ");
+            string accountHolderName = Console.ReadLine();
+
+            Console.Write("Enter your Bank Name: ");
+            string bankName = Console.ReadLine();
+
+            double openingBalance;
             do
             {
-                Console.WriteLine("\nMenu:");
-                Console.WriteLine("1. Deposit");
-                Console.WriteLine("2. Withdraw");
-                Console.WriteLine("3. Display Balance");
-                Console.WriteLine("4. Exit");
-                Console.Write("Enter your choice: ");
+                Console.Write("Enter Opening Balance (minimum 500 rupees): ");
+            } while (!double.TryParse(Console.ReadLine(), out openingBalance) || openingBalance < 500);
 
-                if (int.TryParse(Console.ReadLine(), out choice))
+            Account account = new Account(accountNumber, accountHolderName, bankName, openingBalance);
+            AccountManager.SaveAccount(account);
+
+            Console.WriteLine("Account created successfully!");
+        }
+
+        private void PrintMenu()
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Display Balance");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+        }
+
+        private int GetUserChoice()
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice))
+            {
+                Console.WriteLine("Invalid choice. Please enter a valid number.");
+            }
+            return choice;
+        }
+
+        private void DepositMoney(Account account)
+        {
+            Console.Write("Enter deposit amount: ");
+            if (double.TryParse(Console.ReadLine(), out double depositAmount))
+            {
+                AccountManager.Deposit(account, depositAmount);
+                AccountManager.SaveAccount(account);
+                Console.WriteLine($"Deposited {depositAmount} rupees.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount entered.");
+            }
+        }
+
+        private void WithdrawMoney(Account account)
+        {
+            Console.Write("Enter withdrawal amount: ");
+            if (double.TryParse(Console.ReadLine(), out double withdrawalAmount))
+            {
+                try
                 {
-                    switch (choice)
-                    {
-                        case 1:
-                            Console.Write("Enter deposit amount: ");
-                            if (double.TryParse(Console.ReadLine(), out double depositAmount))
-                            {
-                                AccountManager.Deposit(account, depositAmount);
-                                AccountManager.SaveAccount(account);
-                                Console.WriteLine($"Deposited {depositAmount} rupees.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("Invalid amount entered.");
-                            }
-                            break;
-                        case 2:
-                            Console.Write("Enter withdrawal amount: ");
-                            if (double.TryParse(Console.ReadLine(), out double withdrawalAmount))
-                            {
-                                try
-                                {
-                                    AccountManager.Withdraw(account, withdrawalAmount);
-
-                                    AccountManager.SaveAccount(account);
-                                    Console.WriteLine($"Withdrawn {withdrawalAmount} rupees.");
-
-
-
-                                }
-                                catch (InsufficientBalanceException ibe)
-                                {
-                                    Console.WriteLine(ibe.Message);
-                                }
-
-                            }
-                            else
-                            {
-                                Console.WriteLine("Invalid amount entered.");
-                            }
-                            break;
-                        case 3:
-                            Console.WriteLine($"Account balance: {AccountManager.CheckBalance(account)} rupees.");
-                            break;
-                        case 4:
-                            Console.WriteLine("Exiting the application.");
-                            break;
-                        default:
-                            Console.WriteLine("Invalid choice. Please try again.");
-                            break;
-                    }
+                    AccountManager.Withdraw(account, withdrawalAmount);
+                    AccountManager.SaveAccount(account);
+                    Console.WriteLine($"Withdrawn {withdrawalAmount} rupees.");
                 }
-                else
+                catch (InsufficientBalanceException ibe)
                 {
-                    Console.WriteLine("Invalid choice. Please try again.");
+                    Console.WriteLine(ibe.Message);
                 }
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount entered.");
+            }
+        }
 
-            } while (choice != 4);
+        private void DisplayBalance(Account account)
+        {
+            Console.WriteLine($"Account balance: {AccountManager.CheckBalance(account)} rupees.");
         }
     }
 }
+
